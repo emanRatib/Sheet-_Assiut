672587c538bf0cbbb8a4908d9ac34a4ab21eb6d7
Eman Ebeid <115788842+emanRatib@users.noreply.github.com>
2022-10-15 20:28:52 +0200

    Sheet # 1

---
+++ B. Basic Data Types/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x; long long L;
+    char ch ;
+    float f; double d;
+    cin>> x>> L>> ch>> f>> d;
+    cout << x<< "\n"<< L << "\n"<< ch <<"\n"<< f<<"\n"<< d;
+
+
+
+    return 0;
+}
